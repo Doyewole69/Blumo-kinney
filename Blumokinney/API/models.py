9e0ea528a1b535a7f49c9fb5faaa28024e816f24
Doyewole69 <doyewole69@gmail.com>
2022-12-19 02:52:13 +0100

    creation of user model

---
+++ Blumokinney/API/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser,UserManager
+
+# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True,blank=False,null=False)
+    phone_number = models.IntegerField(blank=False, unique=True, null=False)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number','first_name','last_name']
+
+    def __str__(self):
+        return self.email
+
